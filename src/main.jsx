5dadbdccdef7a22834843313c58c159774b8f297
earthmhee <visarut.oran@gmail.com>
2025-03-12 17:05:27 +0700

    update Navbar and product card component

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import "./index.css";
-// import AppRouter from "./routes/AppRouter.jsx";
+import AppRouter from "./routes/AppRouter.jsx";
 import { createRoot } from "react-dom/client";
 import { ClerkProvider } from "@clerk/clerk-react";
 import { ToastContainer } from "react-toastify";
@@ -14,8 +14,8 @@ if (!PUBLISHABLE_KEY) {
 
 createRoot(document.getElementById("root")).render(
   <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
-    <App />
-    {/* <AppRouter /> */}
+    {/* <App /> */}
+    <AppRouter />
     <ToastContainer position="top-center" style={{ zIndex: 9999 }} />
   </ClerkProvider>
 );
