1bb8de2fdb6f2bdb12572de6a8389a0792a3161b
Hirun <108975099+H1run9@users.noreply.github.com>
2025-03-12 11:06:13 +0700

    featuer navbarupdate

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { useState } from "react";
-import "./App.css";
 import { Outlet } from "react-router";
+import Layout from "./layouts/Layouts";
 
 function App() {
 	const [count, setCount] = useState(0);
 
 	return (
 		<>
-			<p className="bg-white-200">header</p>
-			<Outlet />
+			<Layout />
+			{/* <Outlet /> */}
+
 		</>
 	);
 }
