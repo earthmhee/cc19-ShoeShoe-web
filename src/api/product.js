c1ec6f3494907d104cd6b77f3db6801317c11a84
earthmhee <visarut.oran@gmail.com>
2025-03-14 15:30:25 +0700

    Connect ProductDetail with backend and apply new ProductCard styles, created api/product.js to include getProductById

--- src/api/product.js
+++ src/api/product.js
@@ -4,3 +4,25 @@ import axios from "axios";
 export const getAllProduct = async () => {
   return await axios.get(`http://localhost:8001/api/product/show-product`);
 };
+
+// api/product.js
+// export const getProductById = async (id) => {
+//   try {
+//     const response = await axios.get(`http://localhost:8001/api/product/products/${id}`); // Adjust the URL according to your API
+//     return response;
+//   } catch (error) {
+//     throw error;
+//   }
+// };
+
+export const getProductById = async (id) => {
+  if (!id) throw new Error('Product ID is required');
+  
+  try {
+    const response = await axios.get(`http://localhost:8001/api/product/products/${id}`);
+    return response;
+  } catch (error) {
+    console.error('API Error:', error);
+    throw error;
+  }
+};
--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter, Navigate, RouterProvider } from "react-router";
 import useUserStore from "../stores/userStore";
 import App from "../App";
 import Products from "../pages/Products";
+import ProductDetail from "../pages/ProductDetail";
 
 const guestRouter = createBrowserRouter([
 	{
@@ -9,6 +10,7 @@ const guestRouter = createBrowserRouter([
 		element: <App />,
 		children: [
 			{ index: true, element: <Products/> },
+			{ path: "/product/:id", element: <ProductDetail /> },
 			{ path: "/login", element: <p>login</p> },
 			{ path: "/register", element: <p>register</p> },
 			{ path: "*", element: <Navigate to="/login" /> },
